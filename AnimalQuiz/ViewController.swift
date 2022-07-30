9d69219ac5bad2adb5fd7c501f01f2e6545ed1a6
Mehrob <shmehrob16@gmail.com>
2022-07-30 07:15:11 +0500

    Initial Commit

---
+++ AnimalQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimalQuiz
+//
+//  Created by Sharipov Mehrob on 30.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
